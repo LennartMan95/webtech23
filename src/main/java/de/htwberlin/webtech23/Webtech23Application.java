270e6cbda437c95a1f8aa325f474e1afb81ed2dc
lennartmanske <lennartmanske@gmail.com>
2023-04-25 11:36:26 +0200

    init

---
+++ src/main/java/de/htwberlin/webtech23/Webtech23Application.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webtech23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webtech23Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech23/Webtech23ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webtech23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
